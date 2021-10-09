225da9b7d9eccf85df9806ef12fb6e3a26eabe15
alexolpe <alexandreolivepellicer@gmail.com>
2021-10-09 14:20:43 +0200

    S'ha afegit el mètode text_multiline

--- puzzle1.rb
+++ puzzle1.rb
@@ -1,33 +1,15 @@
 require 'i2c/drivers/lcd.rb'
 
 class Rfid
+	def initialize #constructor
+		@display = I2C::Drivers::LCD::Display.new('/dev/i2c-1', 0x27, rows=20, cols=4)
+	end
 	
 	def escriure
-		display = I2C::Drivers::LCD::Display.new('/dev/i2c-1', 0x27, rows=20, cols=4)
-
-		display.clear #neteja la pantalla
+		@display.clear #neteja la pantalla
 		puts "Inserte el mensaje: "
-		mensaje = gets #llegeix de terminal i es guarda a un string
-		arraymensaje = mensaje.chars #transforma el string en array
-		arraymensaje.pop #elimina el caracter del return
-
-		mensajeimpreso=""
-		fila=0
-		
-		# FUNCIONALITAT STRING MULTILINEA:recorre l'array i si es detecta un '*', 
-		# es mostra a la primera fila el missatge previ i es salta de fila
-		arraymensaje.each do |letra|
-			if letra =="*"
-				display.text(mensajeimpreso,fila)
-				fila=fila +1
-				mensajeimpreso=""
-			else
-				mensajeimpreso<<letra
-				display.text(mensajeimpreso,fila)
-			end
-	
-		end 
-
+		mensaje = gets.chomp #llegeix de terminal, es guarda a un string i s'elimina el caràcter del return
+		@display.text_multiline(mensaje) #mètode que permet escriure en les diferent linies del display
 	end
 	
 end
